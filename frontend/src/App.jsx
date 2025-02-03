276be93fbe149fe5c5742ca838aee789f3381e8d
Chris Pring <chrisjlpring@outlook.com>
2025-02-03 15:04:32 +0000

    Set up frontend with React, Vite, TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import TestComponent from "./components/TestComponent";
+
+function App() {
+  return (
+    <div className="p-5">
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TestComponent.jsx
@@ -0,0 +1,7 @@
+function TestComponent() {
+  return (
+    <h1 className="text-2xl font-bold text-blue-500">Frontend is working!</h1>
+  );
+}
+
+export default TestComponent;
